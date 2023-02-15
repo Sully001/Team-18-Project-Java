3550ef10dccf348a860b00659a6dc950cd9d0ec0
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-02-15 14:10:06 +0000

    Working starting files

---
+++ bambi/src/main/java/com/example/bambi/BambiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bambi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambiApplication.class, args);
+    }
+
+}
---
+++ bambi/src/test/java/com/example/bambi/BambiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bambi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
