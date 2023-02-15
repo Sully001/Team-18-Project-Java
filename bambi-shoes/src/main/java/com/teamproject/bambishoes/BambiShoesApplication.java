dea21e7470b40bfbef2ac1d3c813a1e3cb312be5
Suliman Abdulkader <sulimanabdulkader@outlook.com>
2023-02-15 13:29:05 +0000

    Starting Java Files

---
+++ bambi-shoes/src/main/java/com/teamproject/bambishoes/BambiShoesApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.bambishoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambiShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BambiShoesApplication.class, args);
+	}
+
+}
